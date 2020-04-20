151723eb247ff8d5ded282513e3592399c29ac28
Tran Nguyen Quang Huy <huyvip2000ht@gmail.com>
2020-04-20 23:52:18 +0700

    Finish game2_2
    về cơ bản phần khung là xong
    
    thiếu màn hình victory

--- TestByHuy/lesson2.js
+++ TestByHuy/lesson2.js
@@ -11,18 +11,19 @@ let questionVariable = [           //  [previous column, added column]
     [8,2],
     [8,3],
     [9,3],
-    [6,-1],                            // giai đoạn part 2,
-    [8,1],                            //-1 tượng trưng cho __ tens is the same as ___ units
-    [10,-1],                           // 1 tượng trưng cho __ units is the same as ___ tens
-    [16,1],
-    [13,-1],
-    [18,-1]
+    [6,"tens"],                            // giai đoạn part 2,
+    [8,"units"],                            //"tens" tượng trưng cho __ tens is the same as ___ units
+    [10,"tens"],                           //"units" tượng trưng cho __ units is the same as ___ tens
+    [16,"units"],
+    [13,"tens"],
+    [18,"tens"]
 ];
 let indexX=0;                      // [index của questionVariable]
 let indexY=0;
 
 let check1_1=false;                   // [index câu hỏi 1]
 let check1_2=false;                   // [index câu hỏi 2]
+let check2  =false;
 
 function start() {
     ctx.fillRect(0,0,560*(indexX/(questionVariable.length-1)),40);     // vẽ progress bar
@@ -34,8 +35,40 @@ function start() {
     addColumn(questionVariable[indexX][indexY]);
 
 
-    addQuestion(questionVariable[indexX][indexY]);
+    addQuestion1(questionVariable[indexX][indexY]);
 }
+
+function addQuestion2() {
+    if(questionVariable[indexX][1]==="tens"){
+
+        document.getElementById("question2_2tens").style.display="block";
+        document.getElementById("question2_2units").style.display="none";
+
+
+        document.getElementById("tens").innerHTML=questionVariable[indexX][0];
+    }
+    else{
+
+        document.getElementById("question2_2tens").style.display="none";
+        document.getElementById("question2_2units").style.display="block";
+
+        document.getElementById("units").innerHTML= questionVariable[indexX][0]*10;
+    }
+}
+
+
+function start2() {
+    ctx.fillRect(0,0,560*(indexX/(questionVariable.length-1)),40);     // vẽ progress bar
+    ctx.strokeRect(0,0,560,40);
+
+    if(indexX===4){
+        deletePlatform1();
+    }
+
+
+    addQuestion2();
+}
+
 
 function clearColumn() {                                            // xóa các column trên bản đồ
     var list = document.getElementById("column");
@@ -60,66 +93,88 @@ function addColumn(number) {                                    // thêm column 
     }
     console.log('add '+number+' column');
 }
-function addQuestion(number){                                   // thêm câu hỏi nhưng cái này đã làm trên file html
+function addQuestion1(number){                                   // thêm câu hỏi nhưng cái này đã làm trên file html
 
     console.log('add '+number+' question');
 }
-function checkQuestion1(index){                                  // check đáp án đúng chưa, nếu đúng rồi thì next màn
-    var number;
-    if(indexY===0){
-        if(index===1) {
-            number = questionVariable[indexX][indexY];
+
+
+async function checkQuestion2(index) {
+    let number;
+
+    if (index === 'tens') {
+        number = questionVariable[indexX][0] * 10;
+
+        if (number == document.getElementById("inputTens").value) {
+            console.log("tra loi dung roi");
+            await transition();
+            document.getElementById("inputTens").value = '';
+            next2();
+        } else {
+            console.log("sai roi dap an la " + number);
         }
-        else if(index===2){
-            number = questionVariable[indexX][indexY]*10;
+    } else if (index === 'units') {
+        number = questionVariable[indexX][0];
+
+        if (number == document.getElementById("inputUnits").value) {
+            console.log("tra loi dung roi");
+            await transition();
+            document.getElementById("inputUnits").value = '';
+            next2();
+        } else {
+            console.log("sai roi dap an la " + number);
         }
-    }
-    else if(indexY === 1){
-        if(index===1){
-            number = questionVariable[indexX][0] + questionVariable[indexX][1];
-        }
-        else if(index === 2){
-            number = ( questionVariable[indexX][0] + questionVariable[indexX][1] )*10;
-        }
+
     }
 
-    if(index===1) {
+
+}
+
+
+async function checkQuestion1(index) {                                  // check đáp án đúng chưa, nếu đúng rồi thì next màn
+    var number;
+    if (indexY === 0) {
+        if (index === 1) {
+            number = questionVariable[indexX][indexY];
+        } else if (index === 2) {
+            number = questionVariable[indexX][indexY] * 10;
+        }
+    } else if (indexY === 1) {
+        if (index === 1) {
+            number = questionVariable[indexX][0] + questionVariable[indexX][1];
+        } else if (index === 2) {
+            number = (questionVariable[indexX][0] + questionVariable[indexX][1]) * 10;
+        }
+    }
+    if (index === 1) {
         var answer1 = document.getElementById("input1").value;
         if (answer1 == number) {
-            console.log("dap an dung cau 1");
-
-            check1_1=true;
+            console.log("dap an dung cau 1");//
+            check1_1 = true;
         } else {
-            console.log("dap an sai cau 1");
-
-            console.log("dap an dung cau 1 bang: " + number);
-            check1_1=false;
+            console.log("dap an sai cau 1");//
+            console.log("dap an dung cau 1 bang: " + number);//
+            check1_1 = false;
         }
-    }
-    else if(index===2) {
+    } else if (index === 2) {
         var answer2 = document.getElementById("input2").value;
         if (answer2 == number) {
-            console.log("dap an dung cau 2");
-
+            console.log("dap an dung cau 2");//
             check1_2 = true;
         } else {
-            console.log("dap an sai cau 2");
-
-            console.log("dap an dung cau 2 bang: " + number);
+            console.log("dap an sai cau 2");//
+            console.log("dap an dung cau 2 bang: " + number);//
             check1_2 = false;
         }
     }
-        if(check1_1 && check1_2){
-            document.getElementById("input1").value='';
-            document.getElementById("input2").value='';
-
-            check1_1=false;
-            check1_2=false;
-            next1();
-        }
-
-
-
+    if (check1_1 && check1_2) {
+        await transition();
+        document.getElementById("input1").value = '';
+        document.getElementById("input2").value = '';
+        check1_1 = false;
+        check1_2 = false;
+        next1();
+    }
 }
 function isOutOfQuestion(index){                                    // chuyển sang màn hình victory nhưng chưa làm
     if(indexX>questionVariable.length)
@@ -132,17 +187,30 @@ function deletePlatform1() {
     document.getElementById("question2_1Space").style.display="none";
     document.getElementById("question2_2Space").style.display="block";
 
-    console.log(document.getElementById("content2").childNodes);
+    console.log(document.getElementById("content2").childNodes);//
+}
+
+function transition() {
+        console.log("chuyen canh");
+        return new Promise(resolve => setTimeout(resolve, 2000));
+
+}
+
+
+function goOut() {
+
 }
 
 function next2() {
-    if(indexX===4){
-        deletePlatform1();
+
+        indexX++;
+    if(isOutOfQuestion()){
+        goOut();
     }
-
-
-
-
+    else {
+        start2();
+    }
+    
 }
 
 function next1(){                                                    // next màn
@@ -164,10 +232,13 @@ function next1(){                                                    // next mà
             indexX++;
             indexY = 0;
         }
-
+        console.log("indexX="+indexX+", indexY="+indexY);//
 
         if(indexX<=3){
             start();
+        }
+        else {
+            start2();
         }
 
     }
