140aea3e5594dd5161da82e778c9c92ee284a5a7
Tran Nguyen Quang Huy <huyvip2000ht@gmail.com>
2020-04-23 21:25:42 +0700

    close #8, add victory stage

--- TestByHuy/lesson2.js
+++ TestByHuy/lesson2.js
@@ -66,18 +66,62 @@ function clearColumn() {                                            // xóa các
     }
 }
 
-function addColumn(number) {                                    // thêm column theo number
+function addColumn(number) {                                                    // thêm column theo number
+    var indexAdded;
+    if(indexY===0){
+        indexAdded=0;
+    }
+    else {
+        indexAdded=questionVariable[indexX][0];
+    }
+
+  //  console.log("them truoc do: "+indexAdded);
     for(var a=0;a<number;a++) {
         var img = document.createElement("img");
         img.src = "https://i.ibb.co/4S5Yw11/block.png";
+   //     img.src = "../Picture/block";
         var src = document.getElementById("column");
 
+
+        if(((a+indexAdded)%5)===0  &&  a!==0)
+            img.style.marginLeft="10px";
         src.appendChild(img);
+
+
+
+      //  src[5].style.marginLeft="10px";
     }
 }
+
+function addColumn2(number) {
+    var src = document.getElementById("column2_2");
+    if(!src.hasChildNodes()) {
+        for (var a = 0; a < number; a++) {
+            var img = document.createElement("img");
+            img.src = "https://i.ibb.co/4S5Yw11/block.png";
+
+
+            if ((a % 5) === 0 && a !== 0)
+                img.style.marginLeft = "10px";
+            src.appendChild(img);
+
+
+        }
+    }
+}
+function clearColumn2() {                                            // xóa các column trên bản đồ
+    var list = document.getElementById("column2_2");
+    while (list.hasChildNodes()){
+        list.removeChild(list.firstChild);
+    }
+}
+
+
 function addQuestion1(number){                                   // thêm câu hỏi nhưng cái này đã làm trên file html
 
 }
+
+
 
 async function checkQuestion2(index) {
     let number;
@@ -87,28 +131,79 @@ async function checkQuestion2(index) {
 
         if (number == document.getElementById("inputTens").value) {
             console.log("tra loi dung roi");
+            unShowSuggestion2_2();
             await transition();
             document.getElementById("inputTens").value = '';
             next2();
         } else {
-            console.log("sai roi dap an la " + number);
+            if(number.toString().length===document.getElementById("inputTens").value.length){
+                console.log("sai roi dap an la " + number);
+                showSuggestion2_2(index);
+            }
+
         }
     } else if (index === 'units') {
         number = questionVariable[indexX][0];
 
         if (number == document.getElementById("inputUnits").value) {
             console.log("tra loi dung roi");
+            unShowSuggestion2_2();
             await transition();
             document.getElementById("inputUnits").value = '';
             next2();
         } else {
-            console.log("sai roi dap an la " + number);
+            if(number.toString().length===document.getElementById("inputUnits").value.length){
+                console.log("sai roi dap an la " + number);
+                showSuggestion2_2(index);
+            }
+
         }
     }
 }
 
+function showSuggestion2_2(index) {
+    if(index === 'tens'){
+        document.getElementById("suggest2_2tens").style.display="none";
+        document.getElementById("suggest2_2units").style.display="block";
+        addColumn2(questionVariable[indexX][0]);
+    }
+    else if(index === 'units'){
+        document.getElementById("suggest2_2tens").style.display="block";
+        document.getElementById("suggest2_2units").style.display="none";
+        addColumn2(questionVariable[indexX][0]);
+    }
+}
+
+function unShowSuggestion2_2() {
+    document.getElementById("suggest2_2tens").style.display="none";
+    document.getElementById("suggest2_2units").style.display="none";
+    clearColumn2();
+}
+
+function showSuggestion2_1(index){
+    if(index===1){
+        document.getElementById("suggest2_1units").style.display="none";
+        document.getElementById("suggest2_1tens").style.display="block";
+    }
+    else{
+        if(indexY===0)
+            document.getElementById("tens2_1").innerHTML=questionVariable[indexX][0];
+        else
+            document.getElementById("tens2_1").innerHTML=questionVariable[indexX][0]+questionVariable[indexX][1];
+
+        document.getElementById("suggest2_1units").style.display="block";
+        document.getElementById("suggest2_1tens").style.display="none";
+
+
+    }
+}
+function unShowSuggestion2_1(){
+    document.getElementById("suggest2_1units").style.display="none";
+    document.getElementById("suggest2_1tens").style.display="none";
+}
+
 async function checkQuestion1(index) {                                  // check đáp án đúng chưa, nếu đúng rồi thì next màn
-    var number;
+    let number;
     if (indexY === 0) {
         if (index === 1) {
             number = questionVariable[indexX][indexY];
@@ -125,15 +220,27 @@ async function checkQuestion1(index) {                                  // check
     if (index === 1) {
         var answer1 = document.getElementById("input1").value;
         if (answer1 == number) {
+            unShowSuggestion2_1();
             check1_1 = true;
         } else {
+            if(answer1.length===number.toString().length){
+                console.log("chua dung 1");
+                showSuggestion2_1(index);
+            }
+
             check1_1 = false;
         }
     } else if (index === 2) {
         var answer2 = document.getElementById("input2").value;
         if (answer2 == number) {
+            unShowSuggestion2_1();
             check1_2 = true;
         } else {
+            if(answer2.length===number.toString().length){
+                console.log("chua dung 2");
+                showSuggestion2_1(index);
+            }
+
             check1_2 = false;
         }
     }
@@ -147,7 +254,7 @@ async function checkQuestion1(index) {                                  // check
     }
 }
 function isOutOfQuestion(index){                                    // chuyển sang màn hình victory nhưng chưa làm
-    if(indexX>questionVariable.length)
+    if(indexX>=questionVariable.length)
         return true;
     return false;
 }
@@ -155,17 +262,24 @@ function isOutOfQuestion(index){                                    // chuyển 
 function deletePlatform1() {
     document.getElementById("columnSpace").style.display="none";
     document.getElementById("question2_1Space").style.display="none";
+    document.getElementById("suggest2_1Space").style.display="none";
+
+    document.getElementById("suggest2_2Space").style.display="block";
+    document.getElementById("column2_2Space").style.display="block";
     document.getElementById("question2_2Space").style.display="block";
 }
 
 function transition() {
-        return new Promise(resolve => setTimeout(resolve, 2000));
+        return new Promise(resolve => setTimeout(resolve, 500));
 }
 
 function goOut() {
+    console.log("chuyen trang");
+    window.location.replace("../victory.html");
 }
 
 function next2() {
+    console.log(indexX);
     indexX++;
 
     if(isOutOfQuestion()){
