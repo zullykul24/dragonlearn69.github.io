7e100e5c6f29e46148f2c7011b3ea8e81fc73b0e
zullykul24 <zullykulphinh@gmail.com>
2020-05-12 00:06:18 +0700

    unit test game 1

--- cypress/integration/mytest/game_1.spec.js
+++ cypress/integration/mytest/game_1.spec.js
@@ -5,7 +5,6 @@ describe('Game 1 Test', () => {
         cy.visit('https://zullykul24.github.io/dragonlearn69.github.io/Game1/game1.html');
         cy.wait(500)
         cy.get('#startButton').click();
-      
     })
     it('Should uses', () => {
         cy.get('#next_link').should('be.hidden')
@@ -22,15 +21,10 @@ describe('Game 1 Test', () => {
         cy.get('#message_2').should('contain', 'Wrong answer')
         cy.wait(1000)
 
-       
         cy.get('#submit_3').click(); // if not answer
         cy.get('#message_3').should('contain', 'Write your answer')
         cy.wait(1000)
-
-
     })
-
-
     it('Back to menu', () => {
         cy.get('#backButton').click();
       
--- mytest.js
+++ mytest.js
@@ -1,44 +1,92 @@
 
-///Đây chỉ là file test mẫu
-function Calculator()
-{
-  this.add = function(a, b) { return a+b;};
-  this.minus = function(a, b) { return a-b;};
-  this.multiply = function(a, b) { return a*b;};
-  this.divide = function(a,b) {return a/b;} ;
-}
-describe("Cộng trừ", function() {
-    var cal = new Calculator();
-    
-    it("Một với một là hai", function() {
-      expect(2).toBe(cal.add(1,1));
-    });
-    
-    it("Hai với hai là bốn", function() {
-      expect(4).toBe(cal.add(2,2));
-    });
-    
-    it("Năm trừ hai bằng ba", function() {
-      expect(3).toBe(cal.minus(5,2));
-    });
-    
-   });
-    
-   describe("Nhân chia", function() {
-    var cal = new Calculator();
-    
-    it("Năm nhân hai bằng mười", function() {
-      expect(10).toBe(cal.multiply(5,2));
-    });
-    
-    it("Sáu chia hai bằng ba", function() {
-      expect(3).toBe(cal.divide(6,2));
-    });
-   });
-   describe("Kiểm tra kết quả", function(){
-     var cal = new Calculator();
-     it("Excellent", function(){
-       expect(3).toBe(cal.divide(6,2));
 
-     })
+describe("Cộng điểm", function() {
+  function Game()
+{   
+  this.point = 0;
+  this.pos = 0;
+  this.addPoint = function() {
+    
+    var id = setInterval(frame, 5);
+    function frame()
+    {
+        if (this.pos == 360)
+        { 
+            clearInterval(id);
+        }else 
+        {
+            this.pos++;
+        }
+    }
+    this.point++;
+    this.pos = 0;
+  }
+}
+    
+    it("Tăng 1 điểm", function() {
+      var n = new Game();
+      n.addPoint();
+      expect(n.point).toBe(1);
+    });
+    it("Tăng 5 điểm", function() {
+      var n = new Game();
+      for(var i = 0;i<5;i++)n.addPoint();
+      expect(n.point).toBe(5);
+    });
+    
    });
+   describe("Trừ điểm với điểm ban đầu là 5", function() {
+    function Game()
+  {   
+    this.point = 5;
+    this.pos = 0;
+    this.subPoint = function() {
+      
+    var id = setInterval(frame, 5);
+    function frame()
+    {
+        if(pos == 0)
+        {
+            clearInterval(id);
+        }else 
+        {
+            ele.style.left = this.pos + "px";
+            pos--;
+        }
+    }
+    this.point--;
+    this.pos = 360;
+    }
+  }
+      
+      it("Giảm 1 điểm", function() {
+        var n = new Game();
+        n.subPoint();
+        expect(n.point).toBe(4);
+      });
+      it("Tăng 3 điểm", function() {
+        var n = new Game();
+        for(var i = 0;i<3;i++)n.subPoint();
+        expect(n.point).toBe(2);
+      });
+      
+     });
+describe("Hàm kiểm tra đáp án", function () {
+ function Game(){
+   this.numbers1_arr = ["359", "826", "274", "618", "487"];
+   this.check = function(number, index){
+     this.input = this.numbers1_arr[index];
+     if(this.input == number)return true;
+     else return false;
+   }
+ }
+ it("Kiểm tra đầu vào đầu tiên", function () {
+    var n = new Game();
+    expect(n.check(359,0)).toBe(true);   
+ })
+ it("Kiểm tra đầu vào thứ 2", function () {
+  var n = new Game();
+  expect(n.check(359,1)).toBe(false);   
+})
+})    
+   
